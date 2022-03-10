a11fd5be2d34b937df47deccee4ff657224d3c09
wink-thedeeplearner <khai.win2018@gmail.com>
2022-03-10 16:04:09 -0500

    add features used in the ml model

---
+++ src/features.py
@@ -0,0 +1,14 @@
+def get_cat_features():
+    """ Return feature categories
+    """
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    return cat_features
